ea71ae9b339135ed05775453794ebc670f00a989
hgpark <hanguyl91@gmail.com>
2024-08-01 18:43:11 +0900

    feat: use private fields

--- src/index.ts
+++ src/index.ts
@@ -14,19 +14,20 @@ type CacheEntity = {
 };
 
 export class BunSqliteStoreClass implements Store {
-  _ttl: number | undefined;
+  #sqlite: Database;
 
-  sqlite: Database;
+  #ttl: number | undefined;
+  #isCacheable: (value: unknown) => boolean;
 
-  isCacheable: (value: unknown) => boolean;
-
-  STATEMENTS: {
+  #STATEMENTS: {
     select: Statement<CacheEntity, [string]>;
     keys: Statement<{ key: string }, [string]>;
 
     upsert: Statement;
     delete: Statement;
     reset: Statement;
+
+    purge: Statement;
   };
 
   constructor(options?: BunSqliteStoreOptions) {
@@ -38,13 +39,13 @@ export class BunSqliteStoreClass implements Store {
       throw new Error('Missing table name');
     }
 
-    this.isCacheable = options?.isCacheable ?? ((val) => val !== undefined);
+    this.#isCacheable = options?.isCacheable ?? ((val) => val !== undefined);
 
-    this.sqlite = new Database(options.path);
-    this._ttl = options?.ttl;
+    this.#sqlite = new Database(options.path);
+    this.#ttl = options?.ttl;
 
     const tableName = options.name;
-    this.sqlite.exec(`
+    this.#sqlite.exec(`
       CREATE TABLE IF NOT EXISTS ${tableName} (
         'key' TEXT PRIMARY KEY,
         'val' TEXT,
@@ -53,29 +54,33 @@ export class BunSqliteStoreClass implements Store {
       );
       CREATE INDEX IF NOT EXISTS index_expire_cache ON ${tableName}(expired_at);
     `);
-    this.sqlite.exec('PRAGMA journal_mode = WAL;');
+    this.#sqlite.exec('PRAGMA journal_mode = WAL;');
 
-    this.STATEMENTS = {
-      select: this.sqlite.query(`SELECT * FROM ${tableName} WHERE key = ?`),
-      keys: this.sqlite.query(`SELECT key FROM ${tableName} WHERE key LIKE ?`),
-      upsert: this.sqlite.query(
+    this.#STATEMENTS = {
+      select: this.#sqlite.query(`SELECT * FROM ${tableName} WHERE key = ?`),
+      keys: this.#sqlite.query(`SELECT key FROM ${tableName} WHERE key LIKE ?`),
+      upsert: this.#sqlite.query(
         `INSERT OR REPLACE INTO ${tableName} (key, val, created_at, expired_at) VALUES (?, ?, ?, ?)`,
       ),
-      delete: this.sqlite.query(`DELETE FROM ${tableName} WHERE key = ?`),
-      reset: this.sqlite.query(`DELETE FROM ${tableName}`),
+      delete: this.#sqlite.query(`DELETE FROM ${tableName} WHERE key = ?`),
+      reset: this.#sqlite.query(`DELETE FROM ${tableName}`),
+      purge: this.#sqlite.query(
+        `DELETE FROM ${tableName} WHERE expired_at < ?`,
+      ),
     };
   }
 
   async get<T>(key: string): Promise<T | undefined> {
     const ts = Date.now();
-    const result = this.STATEMENTS.select.get(key);
+    const result = this.#STATEMENTS.select.get(key);
+
     if (result && (result.expired_at === -1 || result.expired_at > ts)) {
       return JSON.parse(result.val);
     }
   }
 
   async ttl(key: string): Promise<number> {
-    const result = this.STATEMENTS.select.get(key);
+    const result = this.#STATEMENTS.select.get(key);
     if (!result) {
       return -2;
     }
@@ -86,15 +91,15 @@ export class BunSqliteStoreClass implements Store {
   }
 
   async set<T>(key: string, data: T, ttl?: Milliseconds): Promise<void> {
-    const t = ttl ?? this._ttl;
+    const t = ttl ?? this.#ttl;
     const created_at = Date.now();
     const expired_at = t ? created_at + t : -1;
 
-    if (!this.isCacheable(data)) {
+    if (!this.#isCacheable(data)) {
       throw new Error(`no cacheable value ${JSON.stringify(data)}`);
     }
 
-    this.STATEMENTS.upsert.run(
+    this.#STATEMENTS.upsert.run(
       key,
       JSON.stringify(data),
       created_at,
@@ -103,11 +108,11 @@ export class BunSqliteStoreClass implements Store {
   }
 
   async del(key: string): Promise<void> {
-    this.STATEMENTS.delete.run(key);
+    this.#STATEMENTS.delete.run(key);
   }
 
   async keys(pattern?: string): Promise<string[]> {
-    return this.STATEMENTS.keys
+    return this.#STATEMENTS.keys
       .all(pattern?.replace('*', '%') ?? '%')
       .map((cache) => cache.key);
   }
@@ -115,9 +120,9 @@ export class BunSqliteStoreClass implements Store {
   async mget<T>(...args: string[]): Promise<T[]> {
     const ts = Date.now();
 
-    const trans = this.sqlite.transaction((keys: string[]) => {
+    const trans = this.#sqlite.transaction((keys: string[]) => {
       return keys
-        .map((key) => this.STATEMENTS.select.get(key))
+        .map((key) => this.#STATEMENTS.select.get(key))
         .filter(
           (data) => data && (data.expired_at === -1 || data.expired_at > ts),
         );
@@ -136,18 +141,18 @@ export class BunSqliteStoreClass implements Store {
   }
 
   async mset(args: [string, unknown][], ttl?: Milliseconds): Promise<void> {
-    const t = ttl ?? this._ttl;
+    const t = ttl ?? this.#ttl;
     const created_at = Date.now();
     const expired_at = t ? created_at + t : -1;
 
-    const trans = this.sqlite.transaction(
+    const trans = this.#sqlite.transaction(
       (args: [string, unknown][], created_at: number, expired_at: number) => {
         for (const [key, data] of args) {
-          if (!this.isCacheable(data)) {
+          if (!this.#isCacheable(data)) {
             throw new Error(`no cacheable value ${JSON.stringify(data)}`);
           }
 
-          this.STATEMENTS.upsert.run(
+          this.#STATEMENTS.upsert.run(
             key,
             JSON.stringify(data),
             created_at,
@@ -161,16 +166,16 @@ export class BunSqliteStoreClass implements Store {
   }
 
   async mdel(...args: string[]): Promise<void> {
-    const trans = this.sqlite.transaction((keys: string[]) => {
+    const trans = this.#sqlite.transaction((keys: string[]) => {
       for (const key of keys) {
-        this.STATEMENTS.delete.run(key);
+        this.#STATEMENTS.delete.run(key);
       }
     });
     trans(args);
   }
 
   async reset(): Promise<void> {
-    this.STATEMENTS.reset.run();
+    this.#STATEMENTS.reset.run();
   }
 }
 
